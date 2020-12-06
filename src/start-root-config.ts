395a09cd143037f02119e545193d05dec5770989
omayma.salhi <omayma.salhi@proxym-it.com>
2020-12-06 02:15:58 +0100

    root load angular micro frontend

--- src/start-root-config.ts
+++ src/start-root-config.ts
@@ -1,21 +1,25 @@
-import { registerApplication, start } from "single-spa";
+import {registerApplication, start} from "single-spa";
 import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
+    constructApplications,
+    constructRoutes,
+    constructLayoutEngine,
 } from "single-spa-layout";
 
 const routes = constructRoutes(
-  document.querySelector("#single-spa-layout") as HTMLTemplateElement
+    document.querySelector("#single-spa-layout") as HTMLTemplateElement
 );
 const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
+    routes,
+    loadApp({name}) {
+        return System.import(name);
+    },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
-
+const layoutEngine = constructLayoutEngine({routes, applications});
+registerApplication({
+    name: "start-angular",
+    app: () => System.import("start-angular"),
+    activeWhen: (location) => true
+});
 applications.forEach(registerApplication);
 layoutEngine.activate();
 start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "start";
@@ -30,10 +31,40 @@ module.exports = (webpackConfigEnv, argv) => {
             orgName,
           },
         }),
+        new webpack.LoaderOptionsPlugin({
+          // test: /\.xxx$/, // may apply this only for some modules
+          options: {
+            modules: {
+              rules: [
+                {
+                  test: /\.(png|jpe?g|gif|svg|xml)$/i,
+                  use: [
+                    {
+                      loader: "file-loader",
+                    },
+                  ],
+                },
+                {
+                  test: /\.svg$/,
+                  use: [
+                    {
+                      loader: 'svg-url-loader',
+                      options: {
+                        limit: 10000,
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        }),
       ],
     },
     defaultConfig,
     {
+
+
       // modify the webpack config however you'd like to by adding to this object
     }
   );
