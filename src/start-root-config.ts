33e80a93f285ff49836024234a516419800ce230
omayma.salhi <omayma.salhi@proxym-it.com>
2020-12-10 23:10:05 +0100

    use the html route

--- src/start-root-config.ts
+++ src/start-root-config.ts
@@ -1,25 +1,45 @@
-import {registerApplication, start} from "single-spa";
+import {getAppNames, getAppStatus, registerApplication, start} from "single-spa";
 import {
-    constructApplications,
-    constructRoutes,
-    constructLayoutEngine,
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
 } from "single-spa-layout";
 
-const routes = constructRoutes(
-    document.querySelector("#single-spa-layout") as HTMLTemplateElement
+export const routes = constructRoutes(
+  document.querySelector("#single-spa-layout") as HTMLTemplateElement
 );
-const applications = constructApplications({
-    routes,
-    loadApp({name}) {
-        return System.import(name);
-    },
+export const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    console.warn(name);
+    return System.import(name);
+  },
 });
-const layoutEngine = constructLayoutEngine({routes, applications});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+/*registerApplication({
+  name: "start-angular",
+  app: () => System.import("start-angular"),
+  activeWhen: (location) => true,
+});
 registerApplication({
-    name: "start-angular",
-    app: () => System.import("start-angular"),
-    activeWhen: (location) => true
-});
+  name: "second-angular",
+  app: () => System.import("second-angular"),
+  activeWhen: "/second-angular"
+});*/
+const appNames = getAppNames();
+console.log(appNames); // ['app1', 'app2', 'app3', 'navbar']
+
 applications.forEach(registerApplication);
 layoutEngine.activate();
+console.log(layoutEngine.isActive());
 start();
+
+
+const status = getAppStatus('start-angular');
+const status2 = getAppStatus("second-angular");
+console.log(status);
+console.log(status2);
+
+
+
+
